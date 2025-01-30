09269d83d5858a470b78614e2b4cd01eee5de62c
Dhruvil Patel <85049831+dhruvil-patel009@users.noreply.github.com>
2025-01-30 17:50:48 +0530

    Setup and Create a  Tab

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,35 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { View, Text } from 'react-native'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index"
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={size} color={color}/>
+          )
+        }}
+      />
+      <Tabs.Screen name="AddNew"
+        options={{
+          tabBarLabel: 'Add New',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="plus-square" size={size} color={color} />
+
+          )
+        }} />
+      <Tabs.Screen name="Profile" 
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="user" size={size} color={color}/>
+        )
+      }}/>
+    </Tabs>
+  );
+}
